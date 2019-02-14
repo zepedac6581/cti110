124c9a1b4b496e437364794f3d10d0f6cde9fbc4
zepedac6581 <47460730+zepedac6581@users.noreply.github.com>
2019-02-14 09:54:01 -0500

    Add files via upload
    My first python program. Displaying input.

---
+++ MyFirstPythonProgram_ClaytonZepeda.py
@@ -0,0 +1,8 @@
+# Interaction with the Python interpreter and IDLE exercise 2
+# 1-29-2019
+# CTI-110 Tutorial 1 - My First Python Program
+# Clayton Zepeda
+#
+print('This is test of the Python interpreter.')
+print('This is a test of IDLE.')
+
