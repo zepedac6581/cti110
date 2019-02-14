8377a5d5e38cf4877df66690322039f43a8a0ebe
zepedac6581 <47460730+zepedac6581@users.noreply.github.com>
2019-02-14 09:52:26 -0500

    Add files via upload
    Program for converting pounds to kilograms.

---
+++ P2HW1_PoundsKilograms_ClaytonZepeda.py
@@ -0,0 +1,13 @@
+# Python program that will convert convert pounds to kilograms.
+# 2-5-2019
+# CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Clayton Zepeda
+#
+# Input the pounds value
+# Calculate conversion to kilograms
+# Output the calculation as kilograms
+
+pounds = float(input("Enter the number of pounds: "))
+kilograms = pounds / 2.2046
+print ('The number of kilograms is:',format(kilograms, ',.2f'))
+
